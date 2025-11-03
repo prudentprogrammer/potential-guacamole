da8a7a500a4300c787e8041028f97572e5af3d42
Arjun Bharadwaj <arjunbharadwaj94@gmail.com>
2025-11-03 13:06:16 +0530

    Initial Commit

---
+++ TestSwift2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TestSwift2
+//
+//  Created by Lakshmi Bharadwaj on 11/3/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TestSwift2/TestSwift2App.swift
@@ -0,0 +1,17 @@
+//
+//  TestSwift2App.swift
+//  TestSwift2
+//
+//  Created by Lakshmi Bharadwaj on 11/3/25.
+//
+
+import SwiftUI
+
+@main
+struct TestSwift2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

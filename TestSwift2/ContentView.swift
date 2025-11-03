fcb13a51e8c7a000343971257cf2cf38ee220e92
Arjun Bharadwaj <arjunbharadwaj94@gmail.com>
2025-11-03 18:31:43 +0530

    Add geometryReader and change to position API

--- TestSwift2/ContentView.swift
+++ TestSwift2/ContentView.swift
@@ -5,33 +5,50 @@ struct ContentView: View {
     let ballRadius = 15.0
     @State var ballX = 0.0
     @State var ballY = 0.0
-    @State var ballDx = 5.0
+    @State var ballDx = 0.0
     @State var ballDy = 5.0
     let timer = Timer.publish(every: 0.030, on: .main, in: .common)
-    let paddleHeight = 150.0
+    let paddleHeight = 200.0
     let paddleOffset = 150.0
+    let paddleWidth = 30.0
     @State var playerPaddleY: CGFloat = 0.0
     
     var body: some View {
-        ZStack {
-            Circle()
-                .fill(Color.red)
-                .frame(width: ballRadius * 2, height: ballRadius * 2)
-                .offset(x: ballX, y: ballY)
-            
-            Rectangle()
-                .fill(.black)
-                .frame(width: 30, height: paddleHeight)
-                .offset(x: -paddleOffset, y: playerPaddleY)
-            
-            Rectangle()
-                .fill(.black)
-                .frame(width: 30, height: paddleHeight)
-                .offset(x: paddleOffset)
-            
-        }.onReceive(timer.autoconnect()) {_ in
-            ballX += ballDx
-            ballY += ballDy
+        GeometryReader { geo in
+            let (W, H) = (geo.size.width, geo.size.height)
+            let halfW = W / 2
+            let halfH = H / 2
+            ZStack {
+                Circle()
+                    .fill(Color.red)
+                    .frame(width: ballRadius * 2, height: ballRadius * 2)
+                    .position(x: ballX + halfW, y: ballY + halfH)
+                
+                Rectangle()
+                    .fill(.yellow)
+                    .frame(width: paddleWidth, height: paddleHeight)
+                    .position(x: 30, y: halfH)
+//
+                Rectangle()
+                    .fill(.red)
+                    .frame(width: paddleWidth, height: paddleHeight)
+                    .position(x: W - 30, y: halfH)
+                
+            }.onReceive(timer.autoconnect()) {_ in
+                // Move the ball
+                ballX += ballDx
+                ballY += ballDy
+                
+                // Check for top and bottom walls for collision
+//                let screenHeight = geometry.size.height
+//                let halfHeight = screenHeight / 2
+//                let bottomCollision = (ballY + ballRadius >= halfHeight)
+//                let topCollision    = (ballY - ballRadius <= -halfHeight)
+//                
+//                if bottomCollision || topCollision {
+//                    ballDy *= -1
+//                }
+            }
         }
     }
 }
